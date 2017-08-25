cf602c72ee15efb98f6f37b956826caa19a402d1
akramabdulrahman <blackthrone91@gmail.com>
2017-08-25 11:00:55 +0300

    fixed a bug with redirection upon jurisdiction failure

--- app/Http/Middleware/CheckUserType.php
+++ app/Http/Middleware/CheckUserType.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Support\Facades\Auth;
 use Flash;
+
 class CheckUserType
 {
     /**
@@ -18,16 +19,27 @@ class CheckUserType
     {
 
         $user = Auth::user();
+        if ($user->isServiceProvider()) {
+            if (in_array('serviceProvider', $userType))
+                return $next($request);
 
-        if ($user->isServiceProvider() && in_array('serviceProvider', $userType)) {
-            return $next($request);
-        } else if ($user->isCitizen() && in_array('citizen', $userType)) {
-            return $next($request);
-        } else if ($user->isWorker() && in_array('worker', $userType)) {
-            return $next($request);
+            return redirect()->back();
+        } else if ($user->isCitizen()) {
+            if (in_array('citizen', $userType))
+                return $next($request);
+
+            return redirect()->back();
+        } else if ($user->isWorker()) {
+            if (in_array('worker', $userType))
+                return $next($request);
+
+            return redirect()->back();
         }
-        if ($user->hasRole('admin') && in_array('admin', $userType)) {
-            return $next($request);
+        if ($user->hasRole('admin')) {
+            if (in_array('admin', $userType))
+                return $next($request);
+
+            return redirect()->back();
         }
 
         return redirect('/checkpoint');
